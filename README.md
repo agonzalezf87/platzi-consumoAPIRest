11b86c77b5e1c57561e548646a653cf8565fe1eb
Antonio Gonzalez <agonzalezf87@gmail.com>
2022-05-31 23:45:01 -0500

    Segunda clase endpoints y query parameters

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Curso de consumo de API Rest en Platzi.com
 ## Clase 1: Imágenes random de api The Dog
 - Reto 1: Crear un botón que recargue la imagen. ✅
 - Reto 2: Utilizar async await para realizar el consumo de la imagen. ✅
+
+## Clase 2: Endpoints y query parameters
--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,28 @@
-const URL = 'https://api.thedogapi.com/v1/images/search'
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3'
 
 const dogButton = document.querySelector('#dogButton')
 
 async function getPicture() {
-    let res = await fetch(URL)
-    let dog = await res.json()
+    let res = await fetch(API_URL)
+    let data = await res.json()
 
-    const img = document.querySelector('img')
-    img.src = dog[0].url
+    const img1 = document.getElementById('img1')
+    const img2 = document.getElementById('img2')
+    const img3 = document.getElementById('img3')
+    img1.src = data[0].url
+    img2.src = data[1].url
+    img3.src = data[2].url
 }
+
+/* 
+fetch(URL)
+.then(res => res.json())
+.then(data => {
+    const img = document.querySelector('img')
+    img.src = data[0].url
+});
+*/
 
 dogButton.addEventListener('click', getPicture)
 
 getPicture()
-
-/* fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img')
-        img.src = data[0].url
-    }) */
